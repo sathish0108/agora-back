13383fce58d132f84f5b37a4ed8fe4d3d9c6277e
sathish0108 <98386865+sathish0108@users.noreply.github.com>
2023-05-15 15:31:29 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": Path.resolve(__dirname, "./src"),
+    },
+  },
+});
